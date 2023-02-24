e52ba58341e96f31d2c6931864eaa9157c9d3a4a
Mosematic <moselee10@gmail.com>
2023-02-24 23:38:19 +0300

    sorting algorithms

---
+++ swap_ints.c
@@ -0,0 +1,17 @@
+#include "sor.h"
+
+/**
+ * swap_ints - Swap two given intergers
+ * @a: First integer
+ * @b: Second interger
+ */
+
+void swap ints(int *a,int *b)
+{
+
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+
+}
